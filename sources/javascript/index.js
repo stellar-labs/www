7e3271dbd4ac5445553148a4db750df3ece95b80
Amin NAIRI <nairi.amin@gmail.com>
2018-04-03 16:24:46 +0200

    :sparkles: added vue initialization script

--- sources/javascript/index.js
+++ sources/javascript/index.js
@@ -1,3 +1,8 @@
 'use strict'
-const windowLoadHandler = () => console.log('such wow')
-window.addEventListener('load', windowLoadHandler, false)
+import App from './vue/App.vue'
+window.addEventListener('load', () => {
+  new Vue({
+    el: '#app',
+    render: h => h(App)
+  })
+})
