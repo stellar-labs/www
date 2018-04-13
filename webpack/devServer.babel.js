4c66d21fe43e456d33e27da3bb8e6084e080f50a
Amin NAIRI <nairi.amin@gmail.com>
2018-04-13 15:52:58 +0200

    :wrench: configuration for webpack dev server

--- webpack/devServer.babel.js
+++ webpack/devServer.babel.js
@@ -1,5 +1,21 @@
 'use strict'
 export default {
+  inline: true,
   contentBase: __dirname + '/docs/',
+  watchContentBase: true,
+  historyApiFallback: {
+    rewrites: [
+      {
+        from: '/^.*$/',
+        to: '/'
+      }
+    ]
+  },
+  host: 'localhost',
+  hotOnly: true,
+  index: 'index.html',
+  open: true,
+  overlay: true,
+  port: 8888,
   watchContentBase: true
 }
