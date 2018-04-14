25d46b87df38b9e13a8daf81f01d4d1201733650
Amin NAIRI <nairi.amin@gmail.com>
2018-04-14 22:43:40 +0200

    :sparkles: added links for the tab & side menu & side nav trigger

--- sources/javascript/vue/App.vue
+++ sources/javascript/vue/App.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
+    <materialize-menu-links close-on-click>
+      <materialize-menu-link icon='home'>Home</materialize-menu-link>
+      <materialize-menu-link to='/about' icon='info'>About</materialize-menu-link>
+    </materialize-menu-links>
     <materialize-header>
       <materialize-header-title>Stellar Labs</materialize-header-title>
+      <materialize-menu-trigger />
+      <materialize-header-links>
+        <materialize-header-link icon='home'>Home</materialize-header-link>
+        <materialize-header-link to='/about' icon='info'>About</materialize-header-link>
+      </materialize-header-links>
     </materialize-header>
     <main>
       <router-view></router-view>
@@ -14,10 +23,20 @@
 'use strict'
 import MaterializeHeader from './components/materialize/Header.vue'
 import MaterializeHeaderTitle from './components/materialize/HeaderTitle.vue'
+import MaterializeHeaderLinks from './components/materialize/HeaderLinks.vue'
+import MaterializeHeaderLink from './components/materialize/HeaderLink.vue'
+import MaterializeMenuLinks from './components/materialize/MenuLinks.vue'
+import MaterializeMenuLink from './components/materialize/MenuLink.vue'
+import MaterializeMenuTrigger from './components/materialize/MenuTrigger.vue'
 export default {
   components: {
     MaterializeHeader,
-    MaterializeHeaderTitle
+    MaterializeHeaderTitle,
+    MaterializeHeaderLinks,
+    MaterializeHeaderLink,
+    MaterializeMenuLinks,
+    MaterializeMenuLink,
+    MaterializeMenuTrigger
   },
   data() {
     return {}
