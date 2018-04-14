bb9be28a0982db9b7392e39c9cb48276bc73ee35
Amin NAIRI <nairi.amin@gmail.com>
2018-04-14 21:49:38 +0200

    :sparkles: added materialize header and title components

--- sources/javascript/vue/App.vue
+++ sources/javascript/vue/App.vue
@@ -1,9 +1,24 @@
 <template>
-  <h1>App</h1>
+  <div>
+    <materialize-header>
+      <materialize-header-title>Stellar Labs</materialize-header-title>
+    </materialize-header>
+    <main>
+      <router-view></router-view>
+    </main>
+    <footer>
+    </footer>
+  </div>
 </template>
 <script>
 'use strict'
+import MaterializeHeader from './components/materialize/Header.vue'
+import MaterializeHeaderTitle from './components/materialize/HeaderTitle.vue'
 export default {
+  components: {
+    MaterializeHeader,
+    MaterializeHeaderTitle
+  },
   data() {
     return {}
   }
