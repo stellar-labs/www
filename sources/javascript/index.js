8b715f641136b92c163fff87ac96006f63e34ec0
Amin NAIRI <nairi.amin@gmail.com>
2018-04-14 21:48:43 +0200

    :building_construction: exploded configuration for the router

--- sources/javascript/index.js
+++ sources/javascript/index.js
@@ -1,8 +1,10 @@
 'use strict'
 import App from './vue/App.vue'
+import router from './router.js'
 window.addEventListener('load', () => {
   new Vue({
     el: '#app',
+    router,
     render: h => h(App)
   })
 })
